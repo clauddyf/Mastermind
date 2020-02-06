f004b59b243d7d10b5820f7b767b7dddd090316c
claudius <claudius.solomon@gmail.com>
2020-02-06 15:32:52 -0800

    finally rendering an arr of objects

--- routes/testGenerator.js
+++ routes/testGenerator.js
@@ -3,15 +3,40 @@ const router = express.Router();
 const fetch = require('node-fetch')
 
 router.get('/', function(req, res, next) {
-  const url = 'https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new';
-  res.set('Content-Type', 'application/json')
+  const url = 'https://www.random.org/integers/?num=11&min=1&max=6&col=1&base=10&format=plain&rnd=new';
   fetch(url)
   .then(res => res.text())
-  .then(data => console.log(res))
+  .then(body => {
+    let arr =[];
+
+    for (let i = 1; i <(body.length); i++){
+      if (i % 2 === 0){
+        let obj = {};
+        obj[body[i]] = i;
+        arr.push(obj);
+      }
+    }
+    return (arr)
+   }
+  
+  )
+  .then(body => res.send(body))
 });
 
 
 module.exports = router;
+
+
+// jsonrpc: "2.0",
+//     method: "generateIntegers",
+//     params: {
+//         "apiKey": "6b1e65b9-4186-45c2-8981-b77a9842c4f0",
+//         "n": 6,
+//         "min": 1,
+//         "max": 6,
+//         "replacement": true
+//     },
+//     id: 42
 // let arr = [];
 
 // {
