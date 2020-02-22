46f683a6ab97d9da498c0a8cdc9c0ce41c82e46b
claudius <claudius.solomon@gmail.com>
2020-02-21 21:37:22 -0800

    changed the greeting to a class component so that I can integrate the modal into the greeting

--- frontend/src/greeting.jsx
+++ frontend/src/greeting.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Play from './game_play'
 
-const greeting = () => (
-    <div className= 'header-link'>
-        <div className='logo-img'>
-            <a href="https://www.linkedin.com/in/claudius-solomon-ba3a2494/">
-                <img src="linkedin.png"  alt=""/>
-            </a>
-        </div>
-        <div className='logo-name'>
-            Mastermind
-        </div>
-    </div>
-);
+class Greeting extends React.Component{
+    render(){
+        return (
+            <div className= 'header-link'>
+                <div className='logo-img'>
+                    <a href="https://www.linkedin.com/in/claudius-solomon-ba3a2494/">
+                        <img src="linkedin.png"  alt=""/>
+                    </a>
+                </div>
+                <div className='logo-name'>
+                    Mastermind
+                </div>
+            </div>
+        );
 
-export default greeting;
+    } 
+
+} 
+
+export default Greeting;
--- frontend/src/modal.jsx
+++ frontend/src/modal.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Modal = ({handleClose,show,children}) => {
+const Modal = ({handleClose,show}) => {
     const showHideClassName = show ? "modal display-block": "modal display-none";
 
     return (
         <div className={showHideClassName}>
             <section className='modal-main'>
-                {children}
                 <div className='chooseInstructions'>
                     <h2>Instructions</h2>
                     <ul>
